719e3d5da6e2dadb00105ea7b3c3a89bc25d9f3f
iamvidhi <gargvidhi@gmail.com>
2023-08-11 22:21:03 +0530

    hello

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -6,8 +6,7 @@ const options = {
       videoId: 'M7FIvfx5J10'
     },
     headers: {
-      'X-RapidAPI-Key': process.env.
-      REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
